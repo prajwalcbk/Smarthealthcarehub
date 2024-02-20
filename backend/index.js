cc42ab74b8539dc340b24be6ae75e66313a4fba4
prajwal3819 <prajwal@eltropy.com>
2024-02-21 00:26:38 +0530

    Adding forums

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const login = require('./controllers/login');
+const forums = require('./controllers/forums');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(login);
+app.use(forums);
 
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
